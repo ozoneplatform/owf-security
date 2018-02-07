0e51ea8e3ad2dfc1353eaac8aaf4030b78690f30
Jeff Harlan <jeff.harlan@nextcentury.com>
2018-02-07 14:45:23 -0500

    update Ehcache v2.10.4 → v3.2.3
    Related Issue(s):
    DG-261

--- src/main/java/ozone/security/session/ClusteredSessionRegistryImpl.java
+++ src/main/java/ozone/security/session/ClusteredSessionRegistryImpl.java
@@ -1,31 +1,10 @@
 package ozone.security.session;
 
-/**
- * This class is based on the example code posted at 
- * http://scalejava.blogspot.com/2012/12/clustered-spring-sessionregistry.html.
- *
- * It implements the spring SessionRegistry in a way that can be clustered
- * using ehcache
- */
-
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
-import javax.servlet.http.HttpSession;
-
-import net.sf.ehcache.Cache;
-import net.sf.ehcache.Ehcache;
-import net.sf.ehcache.CacheManager;
-import net.sf.ehcache.Element;
-import net.sf.ehcache.event.CacheEventListener;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.ehcache.Cache;
+import org.ehcache.event.CacheEvent;
+import org.ehcache.event.EventFiring;
+import org.ehcache.event.EventOrdering;
+import org.ehcache.event.EventType;
 import org.springframework.context.ApplicationListener;
 import org.springframework.security.core.session.SessionDestroyedEvent;
 import org.springframework.security.core.session.SessionInformation;
@@ -33,16 +12,23 @@ import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.web.session.HttpSessionDestroyedEvent;
 import org.springframework.util.Assert;
 
-import ozone.security.CacheManagerFactory;
+import javax.servlet.http.HttpSession;
+import java.util.*;
+import java.util.function.BiFunction;
 
-public class ClusteredSessionRegistryImpl implements SessionRegistry, ApplicationListener<SessionDestroyedEvent> {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(ClusteredSessionRegistryImpl.class);
+/**
+ * This class is based on the example code posted at
+ * http://scalejava.blogspot.com/2012/12/clustered-spring-sessionregistry.html.
+ *
+ * It implements the spring SessionRegistry in a way that can be clustered
+ * using ehcache
+ */
+public class ClusteredSessionRegistryImpl
+        implements SessionRegistry, ApplicationListener<SessionDestroyedEvent>
+{
 
-    private static final String CACHE_NAME = "sessionIds";
-
-	private CacheManager cacheManager;
-	private Cache sessionIds;
+	private final Cache<String, SessionInformation> sessionCache;
 
     /**
      * Implements the event listening needed to reattach a ClusteredSessionInformation
@@ -51,119 +37,60 @@ public class ClusteredSessionRegistryImpl implements SessionRegistry, Applicatio
      * Unfortunately, the ehcache API requires us to implement a bunch of extra functions
      * for listening to other events that we aren't interested in
      */
-    private class CacheListener implements CacheEventListener {
-        public Object clone() { return this.clone(); }
-        public void dispose() {}
-        public void notifyElementEvicted(Ehcache c, Element e) {}
-        public void notifyElementExpired(Ehcache c, Element e) {}
-        public void notifyElementRemoved(Ehcache c, Element e) {}
-        public void notifyRemoveAll(Ehcache c) {}
+    public ClusteredSessionRegistryImpl(OzoneCacheManager ozoneCacheManager) {
+        this.sessionCache = ozoneCacheManager.getSessionCache();
 
-        /**
-         * Set the sessionregistry on the newly added element.  For elements created
-         * by this node, this should have no effect.  For element received from other
-         * cluster nodes, this sets the registry to allow changes to be sent back
-         */
-        private void setSessionRegistry(Element element) {
-            ClusteredSessionInformation info = (ClusteredSessionInformation)element.getValue();
-
-            if (!info.hasSessionRegistry()) {
-                info.setSessionRegistry(ClusteredSessionRegistryImpl.this);
-                LOGGER.debug("setting session registry on element- " + info.getSessionId());
-            }
-        }
-
-        public void notifyElementPut(Ehcache cache, Element element) {
-            setSessionRegistry(element);
-        }
-
-        public void notifyElementUpdated(Ehcache c, Element element) {
-            setSessionRegistry(element);
-        }
+        this.sessionCache.getRuntimeConfiguration().registerCacheEventListener(
+                this::onCacheEvent,
+                EventOrdering.ORDERED,
+                EventFiring.ASYNCHRONOUS,
+                EnumSet.of(EventType.CREATED, EventType.UPDATED));
     }
 
-    public ClusteredSessionRegistryImpl() {
-        cacheManager = CacheManagerFactory.getCacheManager();
+    /**
+     * Set the sessionregistry on the newly added element.  For elements created
+     * by this node, this should have no effect.  For element received from other
+     * cluster nodes, this sets the registry to allow changes to be sent back
+     */
+    private void onCacheEvent(CacheEvent event) {
+        ClusteredSessionInformation info = (ClusteredSessionInformation) event.getNewValue();
 
-        sessionIds = cacheManager.getCache(CACHE_NAME);
-        sessionIds.getCacheEventNotificationService().registerListener(this.new CacheListener());
+        if (info.hasSessionRegistry()) return;
+
+        info.setSessionRegistry(this);
     }
 
 	@Override
 	public void onApplicationEvent(SessionDestroyedEvent event) {
-		LOGGER.debug("onApplicationEvent");
-		if (event instanceof HttpSessionDestroyedEvent) {
-			String sessionId = ((HttpSession) event.getSource()).getId();
-			removeSessionInformation(sessionId);
-		}
-	}
+        if (!(event instanceof HttpSessionDestroyedEvent)) return;
+
+        String sessionId = ((HttpSession) event.getSource()).getId();
+        removeSessionInformation(sessionId);
+    }
 
 	@Override
 	public List<Object> getAllPrincipals() {
-		LOGGER.debug("getAllPrincipals");
-        Map allSessions = sessionIds.getAllWithLoader(sessionIds.getKeys(), null);
-        Set<Object> principals = new HashSet<Object>();
+        Set<Object> principals = mapResultsToSet(sessionCache, (key, info) -> info.getPrincipal());
 
-        for (Object valObj : allSessions.values()) {
-            SessionInformation info = (SessionInformation)valObj;
-            principals.add(info.getPrincipal());
-        }
-        
-        List<Object> principalsList = new ArrayList<Object>(principals.size());
-        principalsList.addAll(principals);
-
-        return principalsList;
+        return new ArrayList<>(principals);
 	}
 
 	@Override
-	public List<SessionInformation> getAllSessions(Object principal, boolean includeExpiredSessions) {
-		LOGGER.debug("Called with includeExpiredSession "+includeExpiredSessions);
+	public List<SessionInformation> getAllSessions(final Object principal, boolean includeExpiredSessions) {
+        return mapResultsToList(sessionCache, (key, info) -> {
+            if (!info.getPrincipal().equals(principal)) return null;
+            if (!includeExpiredSessions && info.isExpired()) return null;
 
-		Set<String> sessionsUsedByPrincipal = getSessionIds(principal);
-		List<SessionInformation> list = new ArrayList<SessionInformation>();
-		Iterator<String> iter = sessionsUsedByPrincipal.iterator();
-
-		while (iter.hasNext()) {
-			String sessionId = iter.next();
-			SessionInformation sessionInformation = getSessionInformation(sessionId);
-
-			if (includeExpiredSessions || !sessionInformation.isExpired()) {
-				list.add(sessionInformation);
-			}
-		}
-		LOGGER.debug("List size "+list.size());
-
-		return list;
+            return info;
+        });
 	}
-	
-    private Set<String> getSessionIds(Object principal) {
-        LOGGER.debug("sessionIds.getKeys(): " + sessionIds.getKeys());
-        Map<Object, SessionInformation> collection = sessionIds.getAllWithLoader(sessionIds.getKeys(), null);
-        Set<String> ids = new HashSet<String>();
-
-        for (SessionInformation info : collection.values()) {
-            if (info != null && info.getPrincipal().equals(principal)) {
-                ids.add(info.getSessionId());
-            }
-        }
-
-        return ids;
-    }
 	
 	@Override
 	public SessionInformation getSessionInformation(String sessionId) {
 		Assert.hasText(sessionId, "SessionId required as per interface contract");
-    	Element element = sessionIds.get(sessionId);
-    	if(element == null) {
-    		LOGGER.debug("Session was null");
-    		return null;
-    	}
-    	else{
-    		SessionInformation session = (SessionInformation) element.getValue();
-    		LOGGER.debug("getSessionInformation: Returning session: "+session.getSessionId() + " principal  "+session.getPrincipal()+ " session expired "+session.isExpired());
-    		return session;
-    	}
-	}
+
+        return sessionCache.get(sessionId);
+    }
 	
 	@Override
 	public void refreshLastRequest(String sessionId) {
@@ -177,29 +104,57 @@ public class ClusteredSessionRegistryImpl implements SessionRegistry, Applicatio
 	public void registerNewSession(String sessionId, Object principal) {
 		Assert.hasText(sessionId, "SessionId required as per interface contract");
 		Assert.notNull(principal, "Principal required as per interface contract");
-		LOGGER.debug("registerNewSession: registering session "+sessionId+" principal "+principal);
-		
+
 		if (getSessionInformation(sessionId) != null) {
-			LOGGER.debug("registerNewSession: Calling remove session");
 			removeSessionInformation(sessionId);
 		}
 		
-        putSessionInformation(new ClusteredSessionInformation(principal, sessionId, new Date(), 
-            this));
+        putSessionInformation(new ClusteredSessionInformation(principal, sessionId, new Date(), this));
 	}
 
-	
 	@Override
 	public void removeSessionInformation(String sessionId) {
 		Assert.hasText(sessionId, "SessionId required as per interface contract");
-		LOGGER.debug("About to remove session "+sessionId);
 
-        sessionIds.remove(sessionId);
+        sessionCache.remove(sessionId);
 	}
 
+    /**
+     * @return a new Set containing all non-null values returned by {@code mapper}
+     */
+    private static <K, V, R> Set<R> mapResultsToSet(Cache<K, V> cache, BiFunction<K, V, R> mapper) {
+        Set<R> results = new HashSet<>();
+        for (Cache.Entry<K, V> entry : cache) {
+            V value = entry.getValue();
+            if (value != null) {
+                R result = mapper.apply(entry.getKey(), value);
+                if (result != null) {
+                    results.add(result);
+                }
+            }
+        }
+        return results;
+    }
+
+    /**
+     * @return a new List containing all non-null values returned by {@code mapper}
+     */
+    private static <K, V, R> List<R> mapResultsToList(Cache<K, V> cache, BiFunction<K, V, R> mapper) {
+        List<R> results = new ArrayList<>();
+        for (Cache.Entry<K, V> entry : cache) {
+            V value = entry.getValue();
+            if (value != null) {
+                R result = mapper.apply(entry.getKey(), value);
+                if (result != null) {
+                    results.add(result);
+                }
+            }
+        }
+        return results;
+    }
+
     private void putSessionInformation(SessionInformation info) {
-		Element element = new Element (info.getSessionId(), info);
-		sessionIds.put(element);
+		sessionCache.put(info.getSessionId(), info);
     }
 
     /**
@@ -212,14 +167,13 @@ public class ClusteredSessionRegistryImpl implements SessionRegistry, Applicatio
      * describe a session that is already present
      */
     private void updateSessionInformation(SessionInformation info) {
-        LOGGER.debug("Updating session info for " + info.getSessionId());
         SessionInformation oldInfo = getSessionInformation(info.getSessionId());
+
         if (oldInfo == null) {
             throw new IllegalArgumentException();
         }
-        else {
-            putSessionInformation(info);
-        }
+
+        putSessionInformation(info);
     }
 
     /**
@@ -227,41 +181,40 @@ public class ClusteredSessionRegistryImpl implements SessionRegistry, Applicatio
      * back to the cache so that they show up throughout the cluster
      */
     private static class ClusteredSessionInformation extends SessionInformation {
-        //the containing instance.  This is done as an explicit variable, instead
-        //of making this class non-static, because it needs to handle serialization
+
+        /**
+         * The containing instance.  This is done as an explicit variable, instead
+         * of making this class non-static, because it needs to handle serialization.
+         */
         private transient ClusteredSessionRegistryImpl sessionRegistry;
 
-        public ClusteredSessionInformation(Object principal, String sessionId, Date lastRequest,
-                ClusteredSessionRegistryImpl sessionRegistry) {
+        ClusteredSessionInformation(Object principal, String sessionId, Date lastRequest,
+                                    ClusteredSessionRegistryImpl sessionRegistry) {
 
             super(principal, sessionId, lastRequest);
             setSessionRegistry(sessionRegistry);
         }
 
-        public void setSessionRegistry(ClusteredSessionRegistryImpl sessionRegistry) {
+        @Override
+        public void refreshLastRequest() {
+            super.refreshLastRequest();
+            sessionRegistry.updateSessionInformation(this);
+        }
+
+        @Override
+        public void expireNow() {
+            super.expireNow();
+            sessionRegistry.updateSessionInformation(this);
+        }
+
+        void setSessionRegistry(ClusteredSessionRegistryImpl sessionRegistry) {
             this.sessionRegistry = sessionRegistry;
         }
 
-        public boolean hasSessionRegistry() {
+        boolean hasSessionRegistry() {
             return sessionRegistry != null;
         }
 
-        public void refreshLastRequest() {
-            super.refreshLastRequest();
-            push();
-        }
-
-
-        public void expireNow() {
-            super.expireNow();
-            push();
-        }
-
-        /**
-         * Updates this SessionInformation in the SessionRegistry
-         */
-        private void push() {
-            sessionRegistry.updateSessionInformation(this);
-        }
     }
+
 }
